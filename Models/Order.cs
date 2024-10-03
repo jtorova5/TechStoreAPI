211b7aea16e03acd11762e53f420309748fee1c8
jtorova5 <jtorova5@gmail.com>
2024-10-03 10:35:38 -0500

    fix: changing bool for string in Status variable in Order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -3,7 +3,7 @@ namespace TechStoreAPI.Models;
 public class Order
 {
     public int Id { get; set; }
-    public bool Status { get; set; }
+    public string? Status { get; set; }
     public DateTime DateOrder { get; set; }
     public int QuantityProduct { get; set; }
     public int IdClient { get; set; }
