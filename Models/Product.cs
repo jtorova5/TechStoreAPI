eaf773508df0b8e61ec9e219ea2244a5d293a125
jtorova5 <jtorova5@gmail.com>
2024-10-03 10:05:10 -0500

    feat: adding properties into Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,5 +2,10 @@ namespace TechStoreAPI.Models;
 
 public class Product
 {
-
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public string? Description { get; set; }
+    public int Quantity { get; set; }
+    public int IdCategory { get; set; }
 }
