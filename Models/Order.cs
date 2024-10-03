0c4042c4ff0792cb206b0ba6e897b08e7b30e128
jtorova5 <jtorova5@gmail.com>
2024-10-03 10:03:31 -0500

    feat: adding properties into Order model

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,5 +2,9 @@ namespace TechStoreAPI.Models;
 
 public class Order
 {
-
+    public int Id { get; set; }
+    public bool Status { get; set; }
+    public DateTime DateOrder { get; set; }
+    public int QuantityProduct { get; set; }
+    public int IdClient { get; set; }
 }
