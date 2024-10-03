259fa88856386252695fa74a9c603c76e45c5017
jtorova5 <jtorova5@gmail.com>
2024-10-03 10:01:36 -0500

    feat: adding properties into Client model

--- Models/Client.cs
+++ Models/Client.cs
@@ -2,5 +2,9 @@ namespace TechStoreAPI.Models;
 
 public class Client
 {
-
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
 }
