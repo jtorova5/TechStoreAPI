701d070db6e1d5032c77a02a7ddffbdc04b8bd13
jtorova5 <jtorova5@gmail.com>
2024-10-03 10:06:47 -0500

    feat: adding properties into User model

--- Models/User.cs
+++ Models/User.cs
@@ -2,5 +2,8 @@ namespace TechStoreAPI.Models;
 
 public class User
 {
-
+    public int Id { get; set; }
+    public int Name { get; set; }
+    public int Password { get; set; }
+    public int IdRole { get; set; }
 }
